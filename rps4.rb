c32302759b3afbfe9a6d81d21f0bebb917deb2d1
Ben Golub <bgolub@lsoc.org>
2016-01-27 14:03:04 -0600

    Almost done, it works, it just does pretty much 3 rounds.

--- rps4.rb
+++ rps4.rb
@@ -3,42 +3,46 @@ wins = 0
 #set loses variable to zero
 loses = 0
 #Prompt user to chose r, p or s
+history = []
 W = 'You win'
 L = 'You lose'
 T = 'You tied'
-while wins != 2 or loses != 2 do
-puts 'Choose Rock(r), Paper(p), or Scissors(s)'
+while wins + loses != 3 do
+  puts 'Choose Rock(r), Paper(p), or Scissors(s)'
 #get user input, set to choice variable, make sure it's lowercase
-choice = gets.chomp.downcase
+  choice = gets.chomp.downcase
 #Select computer choice randomly from arrary, set to variable computer
-
-computer = ['r','p','s'].sample
+  computer = ['r','p','s'].sample
 #Three time do iterate
-case [choice, computer]
-when ['p', 'r'], ['r', 's'], ['s', 'p'],
-  puts W
-  wins = wins + 1
-when ['p', 'p'], ['r', 'r'], ['s', 's'],
-  puts T
-else
-  puts L
-  loses = loses + 1
-
-  #use case statement with [player, computer] (This will be challenging, but totally worth it!)
-    #when ['p', 'r'], ect..
-      #puts you win
-      # add one to win variable
-    #When ['r','r'], ect..
-      #Puts you tied
-    #Else
-      #You Lose
-      # add one to win variable
-end
-#Declare Winner at end with score of wins and loses
-if wins = 2
-  puts W
-else
-  puts L  
+  case [choice, computer]
+    when ['p', 'r'], ['r', 's'], ['s', 'p']
+      puts W
+      wins = wins + 1
+    when ['p', 'p'], ['r', 'r'], ['s', 's']
+      puts T
+    else
+      puts L
+      loses = loses + 1
+    end
+    history << [choice, computer]
+    #use case statement with [player, computer] (This will be challenging, but totally worth it!)
+      #when ['p', 'r'], ect..
+        #puts you win
+        # add one to win variable
+      #When ['r','r'], ect..
+        #Puts you tied
+      #Else
+        #You Lose
+        # add one to win variable
+  end
+  #Declare Winner at end with score of wins and loses
+  if wins == 2
+    puts W + ' the round of 3'
+  else
+    puts L + ' the round of 3' 
+  end  
 #### BONUS ####
   #Show history of computer and user moves at end.
+puts nil
+print history  
 
